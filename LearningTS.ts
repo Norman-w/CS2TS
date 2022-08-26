fc66a3af25b609b32f018fb51edd08b900d9d37f
norman <85535885@qq.com>
2022-08-26 11:14:09 +0800

    still can't got in parameter is extends from type or interface . failed

--- LearningTS.ts
+++ LearningTS.ts
@@ -1,47 +1,97 @@
 //这个文件里面测试一些学习到的ts知识
-
+// function log(obj) {
+//     console.log(obj)
+// }
 //测试方法入参的类型判断(接口)
-interface Person {
+
+
+//人 接口
+interface IPerson {
     Name:string;
     Age:number,
-    Say:()=>void;
+    Say:Function
 }
-interface Animal{
+//人 类型
+type TPerson =
+    {
+        Name:String,
+        Age:number,
+        Say:Function
+    }
+//人 类,继承自接口
+class CPerson implements IPerson
+{
+    Name:'male1';
+    Age:11;
+    public Say:()=>{log(this)}
+}
+
+//人 类,继承自类型
+class CPerson2 implements TPerson
+{
+    Name:String
+    Age:number
+    Say:Function
+    
+    constructor() {
+        this.Say = ():void =>{
+           console.log('this is CPerson2 implements TPerson') 
+        }
+    }
+}
+
+//动物 接口
+interface IAnimal{
     Name:string;
     Age:number;
     Sound:()=>void;
 }
-const norman:Person = {
+
+
+const normanI:IPerson = {
     Name:'norman',
     Age:33,
-    Say:()=>console.log('hello world')
+    Say:()=>console.log('********hello world************')
 }
-const dog:Animal = {
+const normanT:TPerson =
+    {
+        Name:'norman',
+        Age:33,
+        Say:()=>console.log('hello ******* this is person type!!!')
+    }
+const dogI:IAnimal = {
     Name:'tq',
     Age:3,
     Sound:()=>console.log('wang wang!')
 }
 
-function TestWhoAreYou(obj: Person|Animal) {
-    const person:Person = obj as Person;
-    if(obj!= undefined && obj == person)
-    {
-        return PersonSay(obj as Person)
-    }
-    const animal:Animal = obj as Animal;
-    if(typeof obj === typeof animal)
-    {
-        AnimalSound(animal);
-    }
-}
-function PersonSay(person:Person)
+function CallPersonT(person:TPerson)
 {
-    person.Say()
+    person.Say();
 }
-function AnimalSound(animal:Animal)
+function CallPersonI(person:IPerson)
+{
+    // console.log(person.Say)
+    person.Say();
+}
+function CallAnimalI(animal:IAnimal)
 {
     animal.Sound()
 }
 
-TestWhoAreYou(norman);
-TestWhoAreYou(dog);
+function TestWhoAreYou(obj: IPerson|IAnimal|TPerson|CPerson|CPerson2) {
+    //判断是否为某个类的对象 可以使用
+    //obj instanceof Male
+    //或者
+    //obj.constructor == Male
+    
+    //判断是否属于某个类型,可以使用
+    //typeof obj === "number"
+    
+    //todo 判断传入的内容是否继承了接口????what fuck!
+    
+    //todo 怎么判断传入的参数是个对象还是个接口? 怎么判断入参的类型是 类限定类型 还是接口限定类型????
+}
+TestWhoAreYou(normanI);
+// TestWhoAreYou(norman);
+// TestWhoAreYou(dog);
