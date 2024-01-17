86b014b8c980c41f9628ebea7a0e2b2b84bcbe0a
Norman <85535885@qq.com>
2024-01-17 20:31:27 +0800

    对测试代码进行了一些改造,增加了一些注释

--- Tester.cs
+++ Tester.cs
@@ -6,6 +6,24 @@ public static class Tester
 {
 	public static void Test()
 	{
+		#region 准备变量
+
+		//获取当前应用程序的运行路径,通常是在Debug/bin xxx下,AppDomain.CurrentDomain.BaseDirectory;和AppContext.BaseDirectory;都不行
+		// /Users/norman/RiderProjects/CS2TS/bin/Debug/net6.0
+		var currentAppPath = Environment.CurrentDirectory;
+		//使用相对路径"../../../TestCSFiles/Test.cs";
+		var csCodeFilePath = Path.Combine(currentAppPath, "../../../TestCSFiles/Test.cs");
+		//延迟多少秒后开始发送
+		var firstDelay = new TimeSpan(0, 0, 0, 1);
+		//每隔多少秒发送
+		var eachSendDelay = new TimeSpan(0, 0, 0, 0, 500);
+		//发送多少次后退出
+		const int sendTimes = 1000;
+
+		var csCodeString = File.ReadAllText(csCodeFilePath);
+
+		#endregion
+
 		var server = new Server();
 		// //延迟3秒后发送数据(应该连上了)
 		// Task.Run(async () =>
@@ -16,13 +34,12 @@ public static class Tester
 		//延迟1秒后每500毫秒发送一次,大于1000次后退出
 		Task.Run(async () =>
 		{
-			await Task.Delay(1000);
+			await Task.Delay(firstDelay);
 			var i = 0;
-			while (i < 1000)
+			while (i < sendTimes)
 			{
-				await Task.Delay(500);
-				var content = "Hello Norman, this is a test message from C#, sent times: " + i;
-				if (!server.ShowCsCodeString(content)) continue;
+				await Task.Delay(eachSendDelay);
+				if (!server.ShowCsCodeString(csCodeString)) continue;
 				i += 1;
 				Console.WriteLine($"发送了{i}次");
 			}
