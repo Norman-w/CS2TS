01a96a8fe02766e709bb26d241969fd8951d58e0
Norman <85535885@qq.com>
2024-01-18 23:29:00 +0800

    修正客户端协议名称

--- Constant.cs
+++ Constant.cs
@@ -46,17 +46,17 @@ public class Constant
 				/// <summary>
 				///     C# cs代码查看器
 				/// </summary>
-				public const string ControlPanel = "/csCodeViewer";
+				public const string ControlPanel = "controlPannel";
 
 				/// <summary>
 				///     日志查看器,可以通过Logger这个路径来连接一个日志查看器,这样产生日志的时候,就可以通过这个日志查看器来实时查看日志了
 				/// </summary>
-				public const string CsCodeViewer = "/logger";
+				public const string CsCodeViewer = "csCodeViewer";
 
 				/// <summary>
 				///     控制面板,可以通过ControlPanel这个路径来连接一个控制面板,这样就可以通过控制面板来控制服务器了
 				/// </summary>
-				public const string Logger = "/controlPanel";
+				public const string Logger = "logger";
 			}
 		}
 	}
