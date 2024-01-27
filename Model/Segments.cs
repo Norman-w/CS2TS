afd144ce140de556411fd52ba4952b9221ec4688
Norman <85535885@qq.com>
2024-01-27 10:03:12 +0800

    添加TryRemoveWhitespaceBetweenPairSymbol,用于尝试将成对出现的符号,也就是像{},()等这样的符号中间的不可见字符去掉,比如\t呀,空格呀,换行符呀之类的.

--- Model/Segments.cs
+++ Model/Segments.cs
@@ -86,6 +86,99 @@ public static class Segments
 		return currentAfterMergeSegment;
 	}
 
+	/// <summary>
+	///     尝试将成对出现的符号,也就是像{},()等这样的符号中间的不可见字符去掉,比如\t呀,空格呀,换行符呀之类的.
+	/// </summary>
+	/// <returns></returns>
+	public static Segment TryRemoveWhitespaceBetweenPairSymbol(
+		Segment segment,
+		List<Segment> previousSegments,
+		out uint mergeSegmentCount,
+		out uint mergedTotalSegmentCharCount)
+	{
+		//如果现在的符号是一个成对出现的符号的结尾  比如可能是一个括号的右半边 )
+		//那么他的CanInsertContentInMiddle应该是true
+
+		//如果不是这样的符号,那么就不用管了,直接返回
+		//找到以当前这个符号作为右半边的所有可能
+		var endWithThisSegmentStaticPairSegments = All.Where(staticSegment =>
+			staticSegment.Content.EndsWith(segment.Content) && staticSegment.CanInsertContentInMiddle).ToList();
+
+		//如果并没有,那么不是一个成对出现的符号,直接返回
+		if (endWithThisSegmentStaticPairSegments.Count == 0)
+		{
+			mergeSegmentCount = 0;
+			mergedTotalSegmentCharCount = 0;
+			return segment;
+		}
+
+		//向前一直检查到结束
+		var index = previousSegments.Count - 1;
+		//中间隔档的空白符的列表
+		var middleWhitespaceOrLineBreakSegments = new List<Segment>();
+		//目标成对的segment是:
+		var targetPairSegment = Segment.Empty;
+
+		while (index >= 0)
+		{
+			var currentWaitMergeSegment = previousSegments[index];
+
+			//如果不可见假装认为可以吃掉,记录到中间隔档空白符的列表中
+			if (currentWaitMergeSegment.IsWhitespace || currentWaitMergeSegment == LineBreakSymbol)
+			{
+				middleWhitespaceOrLineBreakSegments.Add(currentWaitMergeSegment);
+				index--;
+				continue;
+			}
+			//其他的字符,那就要看是不是组合符号的左半边了,如果不是,那么就不用管了,直接返回
+			//如果是,退出循环,进行下一步的处理
+
+			//假设这个currentWaitMergeSegment就是一个左半边,和右半边也就是传入的segment组合起来,
+			//看一下endWithThisSegmentStaticPairSegments中是否有这样的组合
+			var mergedContent = currentWaitMergeSegment.Content + segment.Content;
+			var matchedStaticSegments =
+				endWithThisSegmentStaticPairSegments.Where(staticSegment => staticSegment.Content == mergedContent);
+			//如果匹配到了多个,那就是错误了,因为一个左半边只能对应一个右半边,抛出异常
+			var staticSegments = matchedStaticSegments as Segment[] ?? matchedStaticSegments.ToArray();
+			switch (staticSegments.Length)
+			{
+				case > 1:
+					throw new Exception(
+						$"Segments:TryRemoveWhitespaceBetweenPairSymbol:一个左半边对应了多个右半边:{string.Join(",", staticSegments.Select(s => s.Content))}");
+				//如果没有匹配到,那就不是一个左半边,直接返回
+				case 0:
+					mergeSegmentCount = 0;
+					mergedTotalSegmentCharCount = 0;
+					return segment;
+			}
+
+			//如果匹配到了且只有一个,那就是这个左半边对应的右半边,退出循环,进行下一步的处理(这个地方staticSegments.Length只能是1)
+			targetPairSegment = staticSegments[0];
+			break;
+		}
+
+		//如果没有找到(也就是index都找到头了),那么就不是一个成对出现的符号,直接返回
+		if (targetPairSegment == Segment.Empty)
+		{
+			mergeSegmentCount = 0;
+			mergedTotalSegmentCharCount = 0;
+			return segment;
+		}
+
+		//否则就是找到了,然后把中间的空白符都吃掉,然后返回
+		if (middleWhitespaceOrLineBreakSegments.Count > 0)
+		{
+			mergeSegmentCount = (uint)middleWhitespaceOrLineBreakSegments.Count;
+			mergedTotalSegmentCharCount = (uint)middleWhitespaceOrLineBreakSegments.Sum(s => s.Length);
+			return targetPairSegment;
+		}
+
+		//如果中间没有空白符,那么就不用管了,直接返回
+		mergeSegmentCount = 0;
+		mergedTotalSegmentCharCount = 0;
+		return segment;
+	}
+
 	#region 一个字符的
 
 	/// <summary>
