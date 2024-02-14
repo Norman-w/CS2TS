2ba70b3bc47b62c330a39904f8f5c84aab764023
Norman <85535885@qq.com>
2024-02-14 23:58:34 +0800

    继续完善Operator,对 + - 等一般操作符 和 implicit/explicit 的参数个数和作用做了区分

--- Model/Node/Operator.cs
+++ Model/Node/Operator.cs
@@ -43,4 +43,32 @@ public class Operator : CodeNode
 	///     如不是implicit或者explicit,那么这个属性就是操作符的名称比如"+"
 	/// </summary>
 	public string? OperatorName { get; set; }
+
+	/// <summary>
+	///     一定不是空的,因为操作符的参数至少有一个
+	///     第一个参数的类型,如果是implicit或者explicit,那么这个属性就是唯一的参数类型(源类型),如:
+	///     public static implicit operator Dollar(Rmb rmb)中的Rmb
+	/// </summary>
+	public string FirstParameterType { get; set; } = string.Empty;
+
+	/// <summary>
+	///     一定不是空的,因为操作符的参数至少有一个
+	///     第一个参数的名称,如果是implicit或者explicit,那么这个属性就是唯一的参数名称(源类型),如:
+	///     public static implicit operator Dollar(Rmb rmb)中的rmb
+	/// </summary>
+	public string FirstParameterName { get; set; } = string.Empty;
+
+	/// <summary>
+	///     第二个参数的类型,如果是implicit或者explicit,那么这个属性就是null,因为implicit和explicit的操作符只有一个参数也就是源类型
+	///     否则就是第二个参数的类型,如:
+	///     public static Dollar operator +(Dollar dollar, Rmb rmb)中的Rmb
+	/// </summary>
+	public string? SecondParameterType { get; set; }
+
+	/// <summary>
+	///     第二个参数的名称,如果是implicit或者explicit,那么这个属性就是null,因为implicit和explicit的操作符只有一个参数也就是源类型
+	///     否则就是第二个参数的名称,如:
+	///     public static Dollar operator +(Dollar dollar, Rmb rmb)中的rmb
+	/// </summary>
+	public string? SecondParameterName { get; set; }
 }
