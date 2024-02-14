fa7dfbf2e988305e94d66fe60e65ad73d2dc4744
Norman <85535885@qq.com>
2024-02-14 09:11:58 +0800

    增加UseForCodeNodeTypes的赋值检查

--- Model/Words/ModifierSegment.cs
+++ Model/Words/ModifierSegment.cs
@@ -2,9 +2,21 @@ namespace CS2TS.Model.Words;
 
 public class ModifierSegment : WordSegment
 {
+	private readonly List<Type> _useForCodeNodeTypes = new();
+
 	/// <summary>
 	///     可以用于哪种类型的CodeNode,比如public等可用于
 	///     public: class/struct/enum/interface/delegate/record/event
 	/// </summary>
-	public List<Type> UseForCodeNodeTypes { get; set; } = new();
+	public List<Type> UseForCodeNodeTypes
+	{
+		get => _useForCodeNodeTypes;
+		set
+		{
+			//检查type是否是CodeNode的子类
+			foreach (var type in value.Where(type => !type.IsSubclassOf(typeof(CodeNode))))
+				throw new Exception(
+					$"类型{type}不是CodeNode的子类,所以不能用于访问修饰符{Content}的CodeNode类型");
+		}
+	}
 }
